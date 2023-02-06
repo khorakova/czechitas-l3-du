343d90744406553b8efa05da8ab20e36ad56c5d7
khorakova <horakoak@gmail.com>
2023-02-06 09:49:56 +0100

    Vypracování úkolu

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 // tady je místo pro náš program
 
+let vystupProgramu = document.querySelector(".vystupProgramu");
+let jmeno = prompt("Jaké je vaše jméno?");
+let prijmeni = prompt("Jaké je vaše příjmení?");
+let rokNarozeni = Number(prompt("Jaký je váš rok narození?"));
+let barva = prompt("Zadej svou oblíbenou barvu");
 
+function vypocetVeku(rokNarozeni) {
+    return 2023 - rokNarozeni;
+} 
+
+vystupProgramu.innerHTML = jmeno + " " + prijmeni + ", " + vypocetVeku(rokNarozeni);
+vystupProgramu.style.color = barva;
 
 
